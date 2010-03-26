3d73d07dc2014d7e40342718e7a928f42221f38a
Matt Robinson <git@nerdoftheherd.com>
2010-03-26 18:31:29 +0000

    Updated the copyright year in the source file headers, and added the header to the new source files.

--- Bitmap.cpp
+++ Bitmap.cpp
@@ -1,3 +1,19 @@
+/**************************************************************************************************************
+  Utility to display BBC Micro / Master Graphics Files.
+  Copyright © 2007-2010 Matt Robinson, © 2003 David Robinson
+ 
+  This program is free software; you can redistribute it and/or modify it under the terms of the GNU General
+  Public License as published by the Free Software Foundation; either version 2 of the License, or (at your
+  option) any later version.
+ 
+  This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
+  implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public
+  License for more details.
+ 
+  You should have received a copy of the GNU General Public License along with this program; if not, write
+  to the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+**************************************************************************************************************/
+
 #include "stdafx.h"
 #include "Bitmap.h"
 
@@ -8,7 +24,7 @@ namespace bmp
 		HBITMAP hBitmap = (HBITMAP)GetCurrentObject(hDC, OBJ_BITMAP);
 
 		// The .BMP file format is as follows:
-		// BITMAPFILEHEADER / BITMAPINFOHEADER / color table / pixel data
+		// BITMAPFILEHEADER / BITMAPINFOHEADER / optional color table / pixel data
 
 		// We need the pixel data and the BITMAPINFOHEADER.
 		// We can get both by using GetDIBits:
@@ -64,9 +80,6 @@ namespace bmp
 			// Then it's followed by the BITMAPINFOHEADER structure
 			WriteFile(hFile, &bmi->bmiHeader, sizeof(BITMAPINFOHEADER),
 						&bytesWritten, NULL);
-	        
-			// Then the colour table.
-			// ...which we don't support yet.
 
 			// Then the pixel data.
 			WriteFile(hFile, pBits, imageBytes, &bytesWritten, NULL);
