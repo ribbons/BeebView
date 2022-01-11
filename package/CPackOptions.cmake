ddfca6532d70debc5f15884cda37ce97fffa3306
Matt Robinson <git@nerdoftheherd.com>
2022-01-11 20:36:44 +0000

    Move author copyright to individual files
    Instead of trying to handle the list of authors separately to the file
    copyright, remove AUTHORS.md and change the copyright in all the files
    to the contributor names.  This helps to keep the author copyright with
    the file content if any of the files are used elsewhere.
    
    This required making some minor adjustments where the build collated a
    list of author names and copyright dates from AUTHORS.md to show in the
    About dialog, Windows .exe properties and .deb copyright - instead just
    show the dates with a hardcoded 'from' year and the year of the latest
    commit as the 'to'.
    
    While updating the copyright headers, also correct a couple of the dates
    as these were incorrect and take the opportunity to reduce the size of
    the file headers by removing the GPL boilerplate from each file and
    replacing it with a SPDX-License-Identifier header line.

--- package/CPackOptions.cmake
+++ package/CPackOptions.cmake
@@ -1,18 +1,6 @@
-# This file is part of BBC Graphics Viewer.
-# Copyright © 2016 by the authors - see the AUTHORS file for details.
+# Copyright © 2016 Matt Robinson
 #
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# SPDX-License-Identifier: GPL-3.0-or-later
 
 if("${CPACK_GENERATOR}" STREQUAL "DEB")
     set(CPACK_COMPONENTS_ALL COMMON LINUX DEBIAN)
