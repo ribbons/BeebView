e02d766ba6d72f139051142defe22d12fb2fc304
Matt Robinson <git@nerdoftheherd.com>
2024-09-22 15:08:20 +0100

    Change CMake component names to lower case
    The component name LINUX was being confused for a variable name by CMake
    under Linux which was causing issues when generating packages.

--- package/CPackOptions.cmake
+++ package/CPackOptions.cmake
@@ -1,13 +1,13 @@
-# Copyright © 2016 Matt Robinson
+# Copyright © 2016-2024 Matt Robinson
 #
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 if("${CPACK_GENERATOR}" STREQUAL "DEB")
-    set(CPACK_COMPONENTS_ALL COMMON LINUX DEBIAN)
+    set(CPACK_COMPONENTS_ALL common linux debian)
 elseif("${CPACK_GENERATOR}" STREQUAL "TGZ")
-    set(CPACK_COMPONENTS_ALL COMMON LINUX ROOTFILES)
+    set(CPACK_COMPONENTS_ALL common linux rootfiles)
 elseif("${CPACK_GENERATOR}" MATCHES "WIX|ZIP")
-    set(CPACK_COMPONENTS_ALL COMMON WINDOWS ROOTFILES)
+    set(CPACK_COMPONENTS_ALL common windows rootfiles)
 else()
-    set(CPACK_COMPONENTS_ALL COMMON)
+    set(CPACK_COMPONENTS_ALL common)
 endif()
