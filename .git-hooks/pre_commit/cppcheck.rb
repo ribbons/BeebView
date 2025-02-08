1919740c5f8d6864cc4aba5ba7f5569c351afe40
Matt Robinson <git@nerdoftheherd.com>
2025-02-08 15:02:31 +0000

    Run cppcheck from an Overcommit pre-commit hook
    cppcheck doesn't handle the includes generated at build time or that
    overriding class functions are not actually unused so suppress those
    checks (and also unmatched suppressions as they only affect some files).

---
+++ .git-hooks/pre_commit/cppcheck.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+# Copyright © 2021-2025 Matt Robinson
+
+module Overcommit
+  module Hook
+    module PreCommit
+      class Cppcheck < Base
+        MESSAGE_REGEX = /^(?<file>.+):(?<line>\d+):/
+
+        def run
+          runcmd = command
+
+          if @context.class.name != 'Overcommit::HookContext::RunAll'
+            runcmd << '--suppress=unusedFunction'
+          end
+
+          result = execute(runcmd, args: applicable_files)
+
+          extract_messages(
+            result.stderr.chomp.split("\n"),
+            MESSAGE_REGEX
+          )
+        end
+      end
+    end
+  end
+end
